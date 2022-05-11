929d6d825a59ec47ac8e8c8cab73336a5bedb356
Egor_Ostroumov <araimo@yandex.ru>
2022-05-11 12:20:38 +0300

    is_isogram

--- is_isogram.py
+++ is_isogram.py
@@ -1,3 +1,11 @@
+"""
+An isogram is a word that has no repeating letters,
+consecutive or non-consecutive. Implement a function that determines whether
+a string that contains only letters is an isogram.
+Assume the empty string is an isogram. Ignore letter case.
+"""
+
+
 def is_isogram(string):
     isogram = string.lower()
     if len(isogram) == len(set(isogram)):
